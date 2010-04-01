21a675fb244bfa121b2725683de084d799e4e5bf
davemckain <davemckain@ded6f4aa-8d4c-4e70-9107-5282a83ebfe5>
2010-04-01 20:56:43 +0000

    Converted to multi-module layout so that samples and diagnostics can be put into separate modules. Contains a basic sub-module for each of these. Another advantage of this is that I can now build a JAR containing both slf4j-nop and slf4j-log4j, which is handy.

---
+++ jacomax-diagnostic/src/main/java/uk/ac/ed/ph/jacomax/diagnostics/JacomaxDiagnostic.java
@@ -0,0 +1,49 @@
+/* $Id: JacomaxDiagnostic.java 5 2010-03-19 15:40:39Z davemckain $
+ *
+ * Copyright (c) 2010, The University of Edinburgh.
+ * All Rights Reserved
+ */
+package uk.ac.ed.ph.jacomax.diagnostics;
+
+import uk.ac.ed.ph.jacomax.JacomaxSimpleConfigurator;
+import uk.ac.ed.ph.jacomax.MaximaConfiguration;
+import uk.ac.ed.ph.jacomax.MaximaInteractiveProcess;
+import uk.ac.ed.ph.jacomax.MaximaProcessLauncher;
+import uk.ac.ed.ph.jacomax.utilities.MaximaOutputUtilities;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Example that does a trivial Maxima call with exceptionally verbose logging.
+ * <p>
+ * (Might be useful for diagnosing and submitting bug reports.)
+ *
+ * @author  David McKain
+ * @version $Revision: 5 $
+ */
+public class JacomaxDiagnostic {
+    
+    private static final Logger logger = LoggerFactory.getLogger(JacomaxDiagnostic.class);
+    
+    public static void main(String[] args) throws Exception {
+        try {
+            MaximaConfiguration configuration = JacomaxSimpleConfigurator.configure();
+            MaximaProcessLauncher launcher = new MaximaProcessLauncher(configuration);
+            MaximaInteractiveProcess process = launcher.launchInteractiveProcess();
+            String result = process.executeCall("1;", 10);
+            String parsed = MaximaOutputUtilities.parseSingleLinearOutputResult(result);
+            process.terminate();
+            if ("1".equals(parsed)) {
+                logger.info("JacomaxDiagnostic ran successfully");
+            }
+            else {
+                logger.error("JacomaxDiagnostic did not work as expected!");
+                logger.info("Raw result was: " + result);
+            }
+        }
+        catch (Exception e) {
+            logger.error("JacomaxDiagnostic throw an Exception when running", e);
+        }
+    }
+}
