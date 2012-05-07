63e4a3135b46d1815b5516409cc302b44e778ebf
David McKain <david.mckain@ed.ac.uk>
2012-05-07 21:34:12 +0100

    Updated copyright & headers

--- jacomax/src/test/java/uk/ac/ed/ph/jacomax/MaximaInteractiveProcessTest.java
+++ jacomax/src/test/java/uk/ac/ed/ph/jacomax/MaximaInteractiveProcessTest.java
@@ -1,7 +1,30 @@
-/* $Id$
- *
- * Copyright (c) 2010, The University of Edinburgh.
- * All Rights Reserved
+/* Copyright (c) 2010 - 2012, The University of Edinburgh.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ * 
+ * * Neither the name of the University of Edinburgh nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package uk.ac.ed.ph.jacomax;
 
@@ -14,8 +37,7 @@ import org.junit.Test;
  *
  * @see MaximaInteractiveProcessOutputTest
  *
- * @author  David McKain
- * @version $Revision$
+ * @author David McKain
  */
 public class MaximaInteractiveProcessTest extends MaximaProcessLauncherTestBase {
 
@@ -82,7 +104,7 @@ public class MaximaInteractiveProcessTest extends MaximaProcessLauncherTestBase 
      */
     @Test
     public void testLisp() throws Exception {
-    	maximaInteractiveProcess.executeCall(":lisp (princ 1)");
+        maximaInteractiveProcess.executeCall(":lisp (princ 1)");
     }
 
     /**
@@ -92,9 +114,9 @@ public class MaximaInteractiveProcessTest extends MaximaProcessLauncherTestBase 
      */
     @Test
     public void testNoNewlineCall() throws Exception {
-    	maximaInteractiveProcess.executeCallDiscardOutput(":lisp (defun $f () (write-char #\\a))");
-    	maximaInteractiveProcess.executeCall("f()$");
-	}
+        maximaInteractiveProcess.executeCallDiscardOutput(":lisp (defun $f () (write-char #\\a))");
+        maximaInteractiveProcess.executeCall("f()$");
+    }
 
     /**
      * Regression test for the fix introduced in revision 18. This makes sure the required call
